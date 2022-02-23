cc98bf5365065c611acb64618d4d0bf7ade565a7
zeus <zeus.ramirez347@gmail.com>
2022-02-23 13:48:24 -0500

    Updated without Bootstrap react and added validation and error handling for inputs

--- accessibility-project-frontend/src/components/BodyForm.js
+++ accessibility-project-frontend/src/components/BodyForm.js
@@ -1,5 +1,3 @@
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
 import { useState } from "react";
 
 
@@ -9,65 +7,73 @@ export default function BodyForm() {
   const [dateOB, setDateOB] = useState(0);
   const [phoneNumber, setPhoneNumber] = useState(0);
   const [emailAddress, setEmailAddress] = useState("");
-  const [zipCode, setZipCode] = useState("");
+  const [zipCode, setZipCode] = useState(0);
+  const [checkedValue, setCheckedValue] = useState(false);
+  const [emailErrors, setEmailErrors] = useState("")
 
   function submitForm(e){
     e.preventDefault()
-    console.log("First name:", firstName, "Last name:", lastName, "Date of Birth:", dateOB, "Phone Number:", phoneNumber, "Zipcode:", zipCode,)
+    checkedValue ? (console.log( firstName, lastName, dateOB, phoneNumber, emailAddress, zipCode)) : (alert("Please check 'I Agree'"))
   }
   return(
 
   <div>
-    <Form>
-      <Form.Group className="mb-3" controlId="formFirstName">
-        <Form.Label>First Name * 
-        <Form.Control type="firstName" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-        </Form.Label>
+    <form>
+      <label>First Name * </label>
+      <input required type="firstName" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+      
+      <br/>
 
-      <Form.Group className="mb-3" controlId="formlastName">
-        <Form.Label>Last Name * 
-        <Form.Control type="lastName" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-        </Form.Label>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Date of Birth * 
-        <Form.Control type="date" name="date_of_birth" onChange={(e) => setDateOB(e.target.value)}/>
-        </Form.Label>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>
-          Email Address 
-          <Form.Control type="text" name="email_address" placeholder="Email Address" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)}/>
-        </Form.Label>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Phone Number 
-        <Form.Control type="tel" pattern="[0-9]*" placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
-        </Form.Label>
-      </Form.Group>
+      <label>Last Name * </label>
+      <input required type="lastName" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+     
+      <br/>
 
-      <Form.Group>
-        <Form.Label>Zipcode  
-        <Form.Control type="tel" pattern="[0-9]*" placeholder="Zipcode" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
-        </Form.Label>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Preferred Language 
-        <Form.Select>
-          <option value="english">English</option>
-          <option value="french">French</option>
-          <option value="german">German</option>
-        </Form.Select>
-        </Form.Label>
-      </Form.Group>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="I Agree" />
-      </Form.Group>
-        <Button onClick={submitForm} variant="primary" type="submit">
-          Submit
-        </Button>
-    </Form>
+      <label>Date of Birth * </label>
+      <input required type="date" maxLength="8" name="date_of_birth" value={dateOB} onChange={(e) => setDateOB(e.target.value)}/>
+
+      <br/>
+
+      <label> Email Address </label>
+      <input type="email" name="email_address" placeholder="Email Address" value={emailAddress} onChange={(e) => {
+        setEmailErrors("")
+        setEmailAddress(e.target.value) 
+        e.target.validity.valid ? console.log(e.target.value) : setEmailErrors(e.target.validationMessage)
+      }
+        }/>
+      
+      {emailErrors.length > 0 ? <><br/><span class="error" aria-live="polite">{emailErrors}</span></> : null }
+
+      <br/>
+
+      <label>Phone Number </label>
+      <input type="tel" maxLength="10" pattern="[0-9]*" placeholder="Phone Number" value={phoneNumber} onChange={(e) => (e.target.validity.valid ? setPhoneNumber(e.target.value) : null ) }/>
+
+      <br/>
+
+      <label>Zipcode</label>
+      <input type="zipcode" maxLength="5" pattern="[0-9]*" placeholder="Zipcode" value={zipCode} onChange={(e) => e.target.validity.valid ? setZipCode(e.target.value): null }/>
+
+      <br/>
+
+      <label>Preferred Language </label>
+      <select>
+        <option value="english">English</option>
+        <option value="french">French</option>
+        <option value="german">German</option>
+      </select>
+
+      <br/>
+
+      <label>I agree</label>
+      <input type="checkbox" onClick={ ()=> setCheckedValue(!checkedValue)} />
+
+      <br/>
+
+      <button onClick={submitForm} variant="primary" type="submit">
+        Submit
+      </button>
+    </form>
   </div>);
 }
 
