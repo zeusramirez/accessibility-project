a7e311730724f9d31855ec49fa9c21babf3ff7b8
zeus <zeus.ramirez347@gmail.com>
2022-03-07 14:41:00 -0500

    Updated webpage to be accessible

--- accessibility-project-frontend/src/components/BodyForm.js
+++ accessibility-project-frontend/src/components/BodyForm.js
@@ -4,60 +4,60 @@ import { useState } from "react";
 export default function BodyForm() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [dateOB, setDateOB] = useState(0);
-  const [phoneNumber, setPhoneNumber] = useState(0);
+  const [dateOB, setDateOB] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
-  const [zipCode, setZipCode] = useState(0);
+  const [zipCode, setZipCode] = useState("");
   const [checkedValue, setCheckedValue] = useState(false);
   const [emailErrors, setEmailErrors] = useState("")
 
   function submitForm(e){
     e.preventDefault()
-    checkedValue ? (console.log( firstName, lastName, dateOB, phoneNumber, emailAddress, zipCode)) : (alert("Please check 'I Agree'"))
+    console.log( firstName, lastName, dateOB, emailAddress, phoneNumber, zipCode)
   }
   return(
 
   <div>
     <form>
-      <label>First Name * </label>
-      <input required type="firstName" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+      <label for="first-name">First Name * </label>
+      <input required={true} id="first-name" type="firstName" aria-label="Enter your first name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
       
       <br/>
 
-      <label>Last Name * </label>
-      <input required type="lastName" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+      <label for="last-name" >Last Name * </label>
+      <input required={true} id="last-name" type="lastName" aria-label="Enter your last name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
      
       <br/>
 
-      <label>Date of Birth * </label>
-      <input required type="date" maxLength="8" name="date_of_birth" value={dateOB} onChange={(e) => setDateOB(e.target.value)}/>
+      <label for="date-ob">Date of Birth * </label>
+      <input required={true} id="date-ob" type="date" maxLength="8" aria-label="Enter your date of birth" name="date_of_birth" value={dateOB} onChange={(e) => setDateOB(e.target.value)}/>
 
       <br/>
 
-      <label> Email Address </label>
-      <input type="email" name="email_address" placeholder="Email Address" value={emailAddress} onChange={(e) => {
+      <label for="email"> Email Address </label>
+      <input id="email" type="email" name="email_address" aria-describedby="email-help" aria-label="Enter your email address" value={emailAddress} onChange={(e) => {
         setEmailErrors("")
         setEmailAddress(e.target.value) 
         e.target.validity.valid ? console.log(e.target.value) : setEmailErrors(e.target.validationMessage)
       }
         }/>
       
-      {emailErrors.length > 0 ? <><br/><span class="error" aria-live="polite">{emailErrors}</span></> : null }
+      {emailErrors.length > 0 ? <><br/><div id="email-help" class="error" aria-live="polite">{emailErrors}</div></> : null }
 
       <br/>
 
-      <label>Phone Number </label>
-      <input type="tel" maxLength="10" pattern="[0-9]*" placeholder="Phone Number" value={phoneNumber} onChange={(e) => (e.target.validity.valid ? setPhoneNumber(e.target.value) : null ) }/>
+      <label for="phone-num">Phone Number </label>
+      <input id="phone-num" type="tel" maxLength="10" pattern="[0-9]*" value={phoneNumber} onChange={(e) => (e.target.validity.valid ? setPhoneNumber(e.target.value) : null ) }/>
 
       <br/>
 
-      <label>Zipcode</label>
-      <input type="zipcode" maxLength="5" pattern="[0-9]*" placeholder="Zipcode" value={zipCode} onChange={(e) => e.target.validity.valid ? setZipCode(e.target.value): null }/>
+      <label for="zipcode">Zipcode</label>
+      <input id="zipcode" type="zipcode" maxLength="5" pattern="[0-9]*" value={zipCode} onChange={(e) => e.target.validity.valid ? setZipCode(e.target.value): null }/>
 
       <br/>
 
-      <label>Preferred Language </label>
-      <select>
+      <label for="language">Preferred Language </label>
+      <select name="language" id="language">
         <option value="english">English</option>
         <option value="french">French</option>
         <option value="german">German</option>
@@ -65,12 +65,12 @@ export default function BodyForm() {
 
       <br/>
 
-      <label>I agree</label>
-      <input type="checkbox" onClick={ ()=> setCheckedValue(!checkedValue)} />
+      <label for="agree">I agree</label>
+      <input id="agree" required={true} type="checkbox" onClick={ (e)=> setCheckedValue(e.target.checked)} />
 
       <br/>
 
-      <button onClick={submitForm} variant="primary" type="submit">
+      <button aria-label="Submit form" onClick={submitForm} variant="primary" type="submit">
         Submit
       </button>
     </form>
